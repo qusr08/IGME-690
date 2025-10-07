4a5dbc99ac6cac7c854f3a90c0ed5ca324ff51b4
Frankie Alfano <falfanoiii@gmail.com>
2025-10-07 19:24:10 -0400

    Got procedural generation of map working

--- Assignment 2/Assets/Scripts/DungeonGenerator.cs
+++ Assignment 2/Assets/Scripts/DungeonGenerator.cs
@@ -1,124 +1,277 @@
-using NUnit.Framework;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class DungeonGenerator : MonoBehaviour
 {
-    [SerializeField] private Vector2Int size;
+    [SerializeField] private Vector3Int size;
 
     private DungeonRoomLibrary roomLibrary;
-    private DungeonRoom[,] map;
-    private DungeonPossibility[,] possibilities;
+    private DungeonEntry[,] map;
 
     private void Awake()
     {
         roomLibrary = GetComponent<DungeonRoomLibrary>();
-        map = new DungeonRoom[size.x, size.y];
-        possibilities = new DungeonPossibility[size.x, size.y];
+        map = new DungeonEntry[size.x, size.z];
     }
 
     private void Start()
     {
-        GenerateMap();
+        StartCoroutine(GenerateMap());
     }
 
-    private void GenerateMap()
+    private IEnumerator GenerateMap()
     {
         // Create a list of all the dungeon possibility entries for the map
         for (int x = 0; x < size.x; x++)
         {
-            for (int y = 0; y < size.y; y++)
+            for (int z = 0; z < size.z; z++)
             {
-                DungeonPossibility possibility = new DungeonPossibility(x, y);
-                possibility.PossibleRooms.AddRange(roomLibrary.DungeonRoomIndices);
-                possibilities[x, y] = possibility;
+                DungeonEntry possibility = new DungeonEntry(x, 0, z);
+                possibility.PossibleRooms.AddRange(roomLibrary.AllDungeonRooms);
+                map[x, z] = possibility;
+                yield return null;
             }
         }
 
-        int collapseCount = 0;
-        while (collapseCount < size.x * size.y)
-        {
-            // Get the positions of the highest entropy on the map
-            List<Vector2Int> highestEntropyPositions = FindHighestEntropyPositions();
+        // Update all of the possible rooms of the map before it starts to generate
+        UpdateEntropyFromOrigin(Vector3Int.zero);
 
-            if (highestEntropyPositions.Count == 0)
+        int collapseCount = 0;
+        while (collapseCount < size.x * size.z)
+        {
+            // Get the positions of the lowest entropy on the map
+            List<Vector3Int> lowestEntropyPositions = FindLowestEntropyPositions();
+
+            // If there are no more positions to collapse, then break from the loop
+            if (lowestEntropyPositions.Count == 0)
             {
                 break;
             }
 
             // Randomly select possibility to resolve
-            Vector2Int collapsePosition = GetRandomListValue(highestEntropyPositions);
+            Vector3Int collapsePosition = lowestEntropyPositions[Random.Range(0, lowestEntropyPositions.Count)];
 
             // Randomly select possible room within that possibility
-            possibilities[collapsePosition.x, collapsePosition.y].CollapsePossibleRoom();
+            map[collapsePosition.x, collapsePosition.z].CollapsePossibleRooms();
+            collapseCount++;
 
             // Update possible rooms for surrounding possibilities
+            UpdateEntropyFromOrigin(collapsePosition);
 
+            // Get the currently collapsed room at the map location
+            DungeonRoom room = map[collapsePosition.x, collapsePosition.z].CollapsedRoom;
 
-            collapseCount++;
+            // If there is no collapsed room there, continue to the next position
+            if (room == null)
+            {
+                continue;
+            }
+
+            Vector3 position = roomLibrary.RoomSize * collapsePosition;
+            Quaternion rotation = Quaternion.Euler(0, room.Orientation.Rotation * -90, 0);
+            Instantiate(room.Prefab, position, rotation, transform);
+
+            yield return new WaitForSeconds(0.1f);
         }
+
+        //// Spawn all room prefabs based on the collapsed rooms
+        //for (int x = 0; x < size.x; x++)
+        //{
+        //    for (int z = 0; z < size.z; z++)
+        //    {
+        //        // Get the currently collapsed room at the map location
+        //        DungeonRoom room = map[x, z].CollapsedRoom;
+
+        //        // If there is no collapsed room there, continue to the next position
+        //        if (room.Prefab == null)
+        //        {
+        //            continue;
+        //        }
+
+        //        Vector3 position = roomLibrary.RoomSize * new Vector3(x, 0, z);
+        //        Quaternion rotation = Quaternion.Euler(0, room.Orientation.Rotation * 90, 0);
+        //        Instantiate(room.Prefab, position, rotation, transform);
+        //    }
+        //}
     }
 
-    private List<Vector2Int> FindHighestEntropyPositions()
+    private List<Vector3Int> FindLowestEntropyPositions()
     {
-        List<Vector2Int> highestEntropyPositions = new List<Vector2Int>();
-        int highestEntropyValue = 0;
+        List<Vector3Int> lowestEntropyPositions = new List<Vector3Int>();
+        int lowestEntropyValue = int.MaxValue;
 
         // Loop through all possibilities
         for (int x = 0; x < size.x; x++)
         {
-            for (int y = 0; y < size.y; y++)
+            for (int z = 0; z < size.z; z++)
             {
                 // Get the current entropy at the position
-                int entropy = possibilities[x, y].Entropy;
+                int entropy = map[x, z].PossibleRooms.Count;
 
-                // If there is a new highest entropy, clear the list and set it as the new highest
-                if (entropy > highestEntropyValue && entropy > 0)
+                // If there is a new lowest entropy, clear the list and set it as the new lowest
+                if (entropy < lowestEntropyValue && entropy > 0)
                 {
-                    highestEntropyPositions.Clear();
-                    highestEntropyValue = entropy;
+                    lowestEntropyPositions.Clear();
+                    lowestEntropyValue = entropy;
                 }
 
-                // Add to the highest entropy list if there is a tie for the highest entropy
-                if (entropy == highestEntropyValue)
+                // Add to the lowest entropy list if there is a tie for the lowest entropy
+                if (entropy == lowestEntropyValue)
                 {
-                    highestEntropyPositions.Add(new Vector2Int(x, y));
+                    lowestEntropyPositions.Add(new Vector3Int(x, 0, z));
                 }
             }
         }
 
-        return highestEntropyPositions;
+        return lowestEntropyPositions;
     }
 
-    private T GetRandomListValue<T>(List<T> list)
+    private void UpdateEntropyFromOrigin(Vector3Int origin)
     {
-        return list[Random.Range(0, list.Count)];
+        // Get a list of the current positions to check
+        List<Vector3Int> updatePositions = new List<Vector3Int>() {
+            origin + Vector3Int.right,
+            origin + Vector3Int.left,
+            origin + Vector3Int.forward,
+            origin + Vector3Int.back
+        };
+        List<Vector3Int> clearedPositions = new List<Vector3Int>()
+        {
+            origin
+        };
+
+        // Keep updating until there are no more positions to update
+        Vector3Int currentPosition;
+        RoomOrientation requirements = new RoomOrientation();
+        int counter = 0;
+        while (updatePositions.Count > 0 && counter < 100)
+        {
+            // Get the next position to update
+            currentPosition = updatePositions[0];
+            updatePositions.RemoveAt(0);
+
+            // If the current position is still in the update position list or has already been cleared, then continue
+            // If the position is outside the bounds of the map, then also continue to the next position
+            if (!IsInsideMap(currentPosition) || updatePositions.Contains(currentPosition) || clearedPositions.Contains(currentPosition))
+            {
+                continue;
+            }
+
+            // At the current position, get data pertaining to the requirements for the room based on surrounding positions
+            requirements.PositiveX = GetOrientation(currentPosition + Vector3Int.right).NegativeX;
+            requirements.NegativeX = GetOrientation(currentPosition + Vector3Int.left).PositiveX;
+            requirements.PositiveZ = GetOrientation(currentPosition + Vector3Int.forward).NegativeZ;
+            requirements.NegativeZ = GetOrientation(currentPosition + Vector3Int.back).PositiveZ;
+
+            // Remove all rooms that do not fit the requirements
+            int removedRooms = map[currentPosition.x, currentPosition.z].RemoveUnfitRooms(requirements);
+
+            // If some possible rooms were removed, then add the surrounding positions to the update list
+            if (removedRooms > 0)
+            {
+                updatePositions.Add(currentPosition + Vector3Int.right);
+                updatePositions.Add(currentPosition + Vector3Int.left);
+                updatePositions.Add(currentPosition + Vector3Int.forward);
+                updatePositions.Add(currentPosition + Vector3Int.back);
+            }
+
+            clearedPositions.Add(currentPosition);
+        }
+    }
+
+    private RoomOrientation GetOrientation(Vector3Int position)
+    {
+        // If the position is outside the bounds of the map, return a default orientation will all of the values set to be walls
+        // This will ensure that any room being collapsed at the edge of the map will have walls surrounding it
+        if (!IsInsideMap(position))
+        {
+            return new RoomOrientation(ConnectionType.WALL);
+        }
+
+        return map[position.x, position.z].CollapsedRoom.Orientation;
+    }
+
+    private bool IsInsideMap(Vector3Int position)
+    {
+        return (position.x >= 0 && position.x < size.x && position.z >= 0 && position.z < size.z);
     }
 }
 
-public class DungeonPossibility
+public class DungeonEntry
 {
-    public List<int> PossibleRooms { get; private set; }
-    public int CollapsedRoom { get; private set; }
-    public bool IsCollapsed => CollapsedRoom > 0;
-    public int Entropy => PossibleRooms.Count;
-    public Vector2Int MapPosition { get; private set; }
+    public List<DungeonRoom> PossibleRooms { get; private set; }
+    public DungeonRoom CollapsedRoom { get; private set; }
+    public Vector3Int MapPosition { get; private set; }
+    public bool IsCollapsed { get; private set; }
 
-    public DungeonPossibility(int x, int y)
+    public DungeonEntry(int x, int y, int z)
     {
-        PossibleRooms = new List<int>();
-        CollapsedRoom = -1;
-        MapPosition = new Vector2Int(x, y);
+        PossibleRooms = new List<DungeonRoom>();
+        CollapsedRoom = new DungeonRoom(null, -1, 0, new RoomOrientation());
+        MapPosition = new Vector3Int(x, y, z);
+        IsCollapsed = false;
     }
 
-    public void CollapsePossibleRoom()
+    public int RemoveUnfitRooms(RoomOrientation requirements)
     {
-        if (IsCollapsed)
+        int removedRooms = 0;
+
+        // Loop through possible rooms for the current position and remove ones that do not match the requirements
+        for (int i = PossibleRooms.Count - 1; i >= 0; i--)
         {
-            return;
+            bool matchPositiveX = requirements.PositiveX == ConnectionType.ANY || PossibleRooms[i].Orientation.PositiveX == requirements.PositiveX;
+            bool matchNegativeX = requirements.NegativeX == ConnectionType.ANY || PossibleRooms[i].Orientation.NegativeX == requirements.NegativeX;
+            bool matchPositiveZ = requirements.PositiveZ == ConnectionType.ANY || PossibleRooms[i].Orientation.PositiveZ == requirements.PositiveZ;
+            bool matchNegativeZ = requirements.NegativeZ == ConnectionType.ANY || PossibleRooms[i].Orientation.NegativeZ == requirements.NegativeZ;
+
+            // If the current room does not match any one of the directions, then 
+            if (!matchPositiveX || !matchNegativeX || !matchPositiveZ || !matchNegativeZ)
+            {
+                PossibleRooms.RemoveAt(i);
+                removedRooms++;
+            }
         }
 
-        CollapsedRoom = PossibleRooms[Random.Range(0, Entropy)];
-        PossibleRooms.Clear();
+        return removedRooms;
+    }
+
+    public DungeonRoom CollapsePossibleRooms()
+    {
+        if (IsCollapsed || PossibleRooms.Count == 0)
+        {
+            return null;
+        }
+
+        // Get a list containing all spawn chances at their corresponding indices
+        List<float> roomSpawnChances = new List<float>();
+        float totalSpawnChanceValue = 0;
+        foreach (DungeonRoom room in PossibleRooms)
+        {
+            roomSpawnChances.Add(room.SpawnChance);
+            totalSpawnChanceValue += room.SpawnChance;
+        }
+
+        // Based on a random value, select a certain room index to be collapsed to
+        float randomValue = Random.Range(0f, 1f);
+        float spawnChanceSum = 0f;
+        for (int i = 0; i < roomSpawnChances.Count; i++)
+        {
+            // Adjust all spawn chances so they add up to 100%
+            spawnChanceSum += roomSpawnChances[i] * (1f / totalSpawnChanceValue);
+
+            if (spawnChanceSum >= randomValue || i == roomSpawnChances.Count - 1)
+            {
+                CollapsedRoom = PossibleRooms[i];
+                IsCollapsed = true;
+                PossibleRooms.Clear();
+
+                return CollapsedRoom;
+            }
+        }
+
+        return null;
     }
 }
--- Assignment 2/Assets/Scripts/DungeonRoomLibrary.cs
+++ Assignment 2/Assets/Scripts/DungeonRoomLibrary.cs
@@ -4,22 +4,30 @@ using UnityEngine;
 
 public enum ConnectionType
 {
-    AIR, WALL
+    ANY, AIR, WALL
 }
 
 public class DungeonRoomLibrary : MonoBehaviour
 {
     [SerializeField] private List<DungeonRoom> _dungeonRooms;
+    [SerializeField, Min(0)] private int _roomSize;
 
     public List<DungeonRoom> DungeonRooms => _dungeonRooms;
-    public List<int> DungeonRoomIndices { get; private set; }
+    public int RoomSize => _roomSize;
+    public List<DungeonRoom> AllDungeonRooms { get; private set; }
 
     private void Awake()
     {
-        DungeonRoomIndices = new List<int>();
-        for (int i = 0; i < DungeonRooms.Count; i++)
+        AllDungeonRooms = new List<DungeonRoom>();
+
+        // Get a list of all dungeon rooms in every possible rotation
+        foreach (DungeonRoom room in DungeonRooms)
         {
-            DungeonRoomIndices.Add(i);
+            RoomOrientation orientation = room.Orientation;
+            for (int j = 0; j < 4; j++)
+            {
+                AllDungeonRooms.Add(new DungeonRoom(room.Prefab, room.Index, room.SpawnChance, orientation.RotateClockwise()));
+            }
         }
     }
 }
@@ -27,11 +35,79 @@ public class DungeonRoomLibrary : MonoBehaviour
 [Serializable]
 public class DungeonRoom
 {
-    public GameObject RoomPrefab;
-    public ConnectionType PositiveXID;
-    public ConnectionType NegativeXID;
-    public ConnectionType PositiveYID;
-    public ConnectionType NegativeYID;
-    public ConnectionType PositiveZID;
-    public ConnectionType NegativeZID;
+    public GameObject Prefab;
+    public int Index;
+    public float SpawnChance;
+    public RoomOrientation Orientation;
+
+    public DungeonRoom(GameObject prefab, int index, float spawnChance, RoomOrientation orientation)
+    {
+        Prefab = prefab;
+        Index = index;
+        SpawnChance = spawnChance;
+        Orientation = orientation;
+    }
+
+    public DungeonRoom(DungeonRoom parent, int rotation)
+    {
+        Prefab = parent.Prefab;
+        Index = parent.Index;
+        SpawnChance = parent.SpawnChance;
+        Orientation = parent.Orientation;
+        Orientation.Rotation = rotation;
+    }
+}
+
+[Serializable]
+public class RoomOrientation
+{
+    public ConnectionType PositiveX;
+    public ConnectionType NegativeX;
+    public ConnectionType PositiveY;
+    public ConnectionType NegativeY;
+    public ConnectionType PositiveZ;
+    public ConnectionType NegativeZ;
+    public int Rotation;
+
+    private ConnectionType defaultType;
+
+    public RoomOrientation(RoomOrientation parent, ConnectionType defaultType = ConnectionType.ANY)
+    {
+        this.defaultType = defaultType;
+        SetValuesFromParent(parent);
+    }
+
+    public RoomOrientation(ConnectionType defaultType = ConnectionType.ANY) : this(null, defaultType) { }
+
+    public RoomOrientation RotateClockwise()
+    {
+        RoomOrientation rotated = new RoomOrientation(this);
+
+        rotated.PositiveX = NegativeZ;
+        rotated.NegativeX = PositiveZ;
+        rotated.PositiveY = PositiveY;
+        rotated.NegativeY = NegativeY;
+        rotated.PositiveZ = PositiveX;
+        rotated.NegativeZ = NegativeX;
+        rotated.Rotation = (Rotation + 1) % 4;
+
+        SetValuesFromParent(rotated);
+        return rotated;
+    }
+
+    public void SetValuesFromParent(RoomOrientation parent)
+    {
+        PositiveX = parent != null ? parent.PositiveX : defaultType;
+        NegativeX = parent != null ? parent.NegativeX : defaultType;
+        PositiveY = parent != null ? parent.PositiveY : defaultType;
+        NegativeY = parent != null ? parent.NegativeY : defaultType;
+        PositiveZ = parent != null ? parent.PositiveZ : defaultType;
+        NegativeZ = parent != null ? parent.NegativeZ : defaultType;
+        Rotation = parent != null ? parent.Rotation : 0;
+    }
+
+    public override string ToString()
+    {
+        return $"+X: {PositiveX} | -X: {NegativeX} | +Y: {PositiveY} | -Y: {NegativeY} | +Z: {PositiveZ} | -Z: {NegativeZ}";
+    }
 }
