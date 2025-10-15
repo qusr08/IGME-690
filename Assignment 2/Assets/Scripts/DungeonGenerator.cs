6a9b27166e341d9cc41f3314a85d883892f5b398
Frankie Alfano <q24xboss@gmail.com>
2025-10-15 02:20:27 -0400

    Added doors to rooms

--- Assignment 2/Assets/Scripts/DungeonGenerator.cs
+++ Assignment 2/Assets/Scripts/DungeonGenerator.cs
@@ -7,10 +7,11 @@ using UnityEngine.UIElements;
 public class DungeonGenerator : MonoBehaviour
 {
 	[SerializeField] private Vector2Int dungeonSize;
-	[SerializeField] private Vector2Int roomMinSize;
-	[SerializeField] private Vector2Int roomMaxSize;
-	[SerializeField] private int roomCount;
 	[SerializeField, Min(0f)] private float generationSpeed;
+	[Space]
+	[SerializeField] private Vector2Int areaMinSize;
+	[SerializeField] private Vector2Int areaMaxSize;
+	[SerializeField, Range(0f, 10f)] private int areaCount;
 
 	private DungeonRoomLibrary roomLibrary;
 	private DungeonEntry[,] map;
@@ -47,11 +48,142 @@ public class DungeonGenerator : MonoBehaviour
 		InitializeRoomPossibilities();
 
 		// Before doing wave function collapse, spawn some starting rooms in the dungeon
-		yield return StartCoroutine(SpawnStartingRooms());
+		yield return StartCoroutine(SpawnStartingAreas());
 
 		// Update all of the possible rooms of the map before it starts to generate
 		UpdateEntropyFromOrigin(Vector2Int.zero, forceEntireMap: true);
 
+		// Use wave function collapse algorithm to create the rest of the dungeon
+		yield return StartCoroutine(CollapseDungeon());
+
+		// Find the largest path through the dungeon and tag all of the rooms that they are part of it
+		yield return StartCoroutine(FindLargestDungeonPath());
+
+		// Remove all rooms that are not connected to the largest dungeon path
+		yield return StartCoroutine(RemoveInvalidRooms());
+	}
+
+	private IEnumerator SpawnStartingAreas ()
+	{
+		List<Vector2Int> availableAreaSizes = new List<Vector2Int>();
+		List<Vector2Int> availableAreaPositions = new List<Vector2Int>();
+		List<Rect> areaRects = new List<Rect>();
+
+		// Get a list of all the possible area sizes
+		for (int x = areaMinSize.x; x <= areaMaxSize.x; x++)
+		{
+			for (int y = areaMinSize.y; y <= areaMaxSize.y; y++)
+			{
+				availableAreaSizes.Add(new Vector2Int(x, y));
+			}
+		}
+
+		// Spawn each of areas into the dungeon
+		while (areaRects.Count < areaCount)
+		{
+			// Select an area size
+			int sizeIndex = Random.Range(0, availableAreaSizes.Count);
+			Vector2Int areaSize = availableAreaSizes[sizeIndex];
+
+			// Get all of the positions that the area can spawn at
+			availableAreaPositions.Clear();
+			for (int x = 1; x < dungeonSize.x - areaSize.x - 1; x++)
+			{
+				for (int y = 1; y < dungeonSize.y - areaSize.y - 1; y++)
+				{
+					// Make sure the current positions is not already within another area
+					Vector2Int position = new Vector2Int(x, y);
+					Rect newAreaRect = new Rect(position, areaSize);
+					if (!areaRects.Where(area => area.Overlaps(newAreaRect)).Any())
+					{
+						availableAreaPositions.Add(position);
+					}
+				}
+			}
+
+			// If there are no available positions for the area to spawn at, then remove that size from the list
+			if (availableAreaPositions.Count == 0)
+			{
+				availableAreaSizes.RemoveAt(sizeIndex);
+				if (availableAreaSizes.Count == 0)
+				{
+					break;
+				}
+
+				continue;
+			}
+
+			// Get a random position for the bottom-left corner of the area
+			Vector2Int areaPosition = availableAreaPositions[Random.Range(0, availableAreaPositions.Count)];
+
+			// Add the area to the area rect list
+			areaRects.Add(new Rect(areaPosition, areaSize));
+
+			// Get the positions of doors around the area
+			// There can be between 1 and 4 doors around the area
+			List<Vector2Int> doorPositions = new List<Vector2Int>();
+			List<int> availableDoorSides = new List<int>() { 0, 1, 2, 3 };
+			int doorCount = Random.Range(1, 5);
+			for (int i = 0; i < doorCount; i++)
+			{
+				// Get a random side for the door to spawn on
+				int sideIndex = Random.Range(0, availableDoorSides.Count);
+				int doorSide = availableDoorSides[sideIndex];
+				availableDoorSides.RemoveAt(sideIndex);
+
+				// Get a position for the door to spawn at
+				Vector2Int sidePosition = Vector2Int.zero;
+				switch (doorSide)
+				{
+					case 0: // Right
+						sidePosition = new Vector2Int(areaSize.x - 1, Random.Range(1, areaSize.y - 2));
+						break;
+					case 1: // Down
+						sidePosition = new Vector2Int(Random.Range(1, areaSize.x - 2), 0);
+						break;
+					case 2: // Left
+						sidePosition = new Vector2Int(0, Random.Range(1, areaSize.y - 2));
+						break;
+					case 3: // Up
+						sidePosition = new Vector2Int(Random.Range(1, areaSize.x - 2), areaSize.y - 1);
+						break;
+				}
+
+				doorPositions.Add(sidePosition + areaPosition);
+			}
+
+			// Collapse all the rooms within the area
+			RoomOrientation requirements = new RoomOrientation();
+			for (int x = areaPosition.x; x < areaPosition.x + areaSize.x; x++)
+			{
+				for (int y = areaPosition.y; y < areaPosition.y + areaSize.y; y++)
+				{
+					Vector2Int currentPosition = new Vector2Int(x, y);
+
+					// Check the surrounding rooms to make sure that all rooms can lead into each other
+					requirements.PositiveX = (x == areaPosition.x + areaSize.x - 1 && GetOrientationAtPosition(currentPosition + Vector2Int.right).NegativeX == ConnectionType.ANY ? ConnectionType.WALL : ConnectionType.AIR);
+					requirements.NegativeX = (x == areaPosition.x && GetOrientationAtPosition(currentPosition + Vector2Int.left).PositiveX == ConnectionType.ANY ? ConnectionType.WALL : ConnectionType.AIR);
+					requirements.PositiveZ = (y == areaPosition.y + areaSize.y - 1 && GetOrientationAtPosition(currentPosition + Vector2Int.up).NegativeZ == ConnectionType.ANY ? ConnectionType.WALL : ConnectionType.AIR);
+					requirements.NegativeZ = (y == areaPosition.y && GetOrientationAtPosition(currentPosition + Vector2Int.down).PositiveZ == ConnectionType.ANY ? ConnectionType.WALL : ConnectionType.AIR);
+
+					// If there is a door at the current position, then make sure all surrounding blocks are air
+					if (doorPositions.Contains(currentPosition))
+					{
+						requirements = new RoomOrientation(ConnectionType.AIR);
+					}
+
+					// With the above requirements, there should only be one room that can be placed
+					map[x, y].RemoveUnfitRooms(requirements);
+					SpawnDungeonRoomPrefab(map[x, y].CollapsePossibleRooms(), currentPosition);
+
+					yield return new WaitForSeconds(generationSpeed);
+				}
+			}
+		}
+	}
+
+	private IEnumerator CollapseDungeon ()
+	{
 		int collapseCount = 0;
 		while (collapseCount < dungeonSize.x * dungeonSize.y)
 		{
@@ -78,6 +210,16 @@ public class DungeonGenerator : MonoBehaviour
 		}
 	}
 
+	private IEnumerator FindLargestDungeonPath ()
+	{
+		yield return null;
+	}
+
+	private IEnumerator RemoveInvalidRooms ()
+	{
+		yield return null;
+	}
+
 	private void InitializeRoomPossibilities ()
 	{
 		map = new DungeonEntry[dungeonSize.x, dungeonSize.y];
@@ -90,83 +232,6 @@ public class DungeonGenerator : MonoBehaviour
 				DungeonEntry possibility = new DungeonEntry(x, y);
 				possibility.PossibleRooms.AddRange(roomLibrary.AllDungeonRooms);
 				map[x, y] = possibility;
-			}
-		}
-	}
-
-	private IEnumerator SpawnStartingRooms ()
-	{
-		List<Vector2Int> availableRoomSizes = new List<Vector2Int>();
-		List<Vector2Int> availableRoomPositions = new List<Vector2Int>();
-		List<Rect> roomRects = new List<Rect>();
-
-		// Get a list of all the possible room sizes
-		for (int x = roomMinSize.x; x <= roomMaxSize.x; x++)
-		{
-			for (int y = roomMinSize.y; y <= roomMaxSize.y; y++)
-			{
-				availableRoomSizes.Add(new Vector2Int(x, y));
-			}
-		}
-
-		// Spawn each of rooms into the dungeon
-		while (roomRects.Count < roomCount)
-		{
-			// Select a room size
-			int sizeIndex = Random.Range(0, availableRoomSizes.Count);
-			Vector2Int roomSize = availableRoomSizes[sizeIndex];
-
-			// Get all of the positions that the room can spawn at
-			availableRoomPositions.Clear();
-			for (int x = 0; x < dungeonSize.x - roomSize.x; x++)
-			{
-				for (int y = 0; y < dungeonSize.y - roomSize.y; y++)
-				{
-					// Make sure the current positions is not already within another room
-					Vector2Int position = new Vector2Int(x, y);
-					Rect newRoomRect = new Rect(position, roomSize);
-					if (!roomRects.Where(room => room.Overlaps(newRoomRect)).Any())
-					{
-						availableRoomPositions.Add(position);
-					}
-				}
-			}
-
-			// If there are no available positions for the room to spawn at, then remove that size from the equation
-			if (availableRoomPositions.Count == 0)
-			{
-				availableRoomSizes.RemoveAt(sizeIndex);
-				if (availableRoomSizes.Count == 0)
-				{
-					break;
-				}
-
-				continue;
-			}
-
-			// Get a random position for the top-left corner of the room
-			Vector2Int roomPosition = availableRoomPositions[Random.Range(0, availableRoomPositions.Count)];
-
-			// Add the room to the room rect list
-			roomRects.Add(new Rect(roomPosition, roomSize));
-
-			// Collapse all the rooms within the area
-			RoomOrientation requirements = new RoomOrientation();
-			for (int x = roomPosition.x; x < roomPosition.x + roomSize.x; x++)
-			{
-				requirements.NegativeX = (x == roomPosition.x ? ConnectionType.WALL : ConnectionType.AIR);
-				requirements.PositiveX = (x == roomPosition.x + roomSize.x - 1 ? ConnectionType.WALL : ConnectionType.AIR);
-
-				for (int y = roomPosition.y; y < roomPosition.y + roomSize.y; y++)
-				{
-					requirements.NegativeZ = (y == roomPosition.y ? ConnectionType.WALL : ConnectionType.AIR);
-					requirements.PositiveZ = (y == roomPosition.y + roomSize.y - 1 ? ConnectionType.WALL : ConnectionType.AIR);
-
-					map[x, y].RemoveUnfitRooms(requirements);
-					SpawnDungeonRoomPrefab(map[x, y].CollapsePossibleRooms(), new Vector2Int(x, y));
-
-					yield return new WaitForSeconds(generationSpeed);
-				}
 			}
 		}
 	}
